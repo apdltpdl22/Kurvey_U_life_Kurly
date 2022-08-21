24927e550b5512f9ee14b8ef92a1dfbec866fe7f
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-08-21 20:25:45 +0900

    :lipstick: 로그인 회원가입 Style 추가

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -67,6 +67,7 @@ export default function SignUp(props) {
     setError(false);
 
     console.log(credentials);
+    
     axios
       .post('url', credentials)
       .then(res => {
@@ -79,55 +80,47 @@ export default function SignUp(props) {
   };
 
   return (
-    <div className={styles.SignUpBox}>
+    <div className={styles.wrap}>
       <h2 className={styles.title}>회원가입</h2>
-      <div>
-        <div>
-          <label htmlFor="input_id">ID: </label>
-          <input
-            ref={id}
-            id="input_id"
-            type="text"
-            name="input_id"
-            value={inputId}
-            onChange={changeId}
-          />
-        </div>
-        <div>
-          <label htmlFor="input_pw">PW: </label>
-          <input
-            ref={pw}
-            id="input_pw"
-            type="password"
-            name="input_pw"
-            value={inputPw}
-            onChange={changePw}
-          />
-        </div>
-        <div>
-          <label htmlFor="input_pw_chk">패스워드 확인: </label>
-          <input
-            ref={pw_chk}
-            id="input_pw_chk"
-            type="password"
-            name="input_pw_chk"
-            value={inputPwChk}
-            onChange={changePwChk}
-          />
-        </div>
-        <div>
-          <label htmlFor="input_name">이름: </label>
-          <input
-            ref={username}
-            id="input_name"
-            type="text"
-            name="input_name"
-            value={inputName}
-            onChange={changeName}
-          />
-        </div>
-        <div>
-          <label htmlFor="input_birthDate">생년월일: </label>
+      <div className={styles.register}>
+        <input
+          ref={id}
+          id="input_id"
+          type="text"
+          name="input_id"
+          placeholder="아이디를 입력해주세요"
+          value={inputId}
+          onChange={changeId}
+        />
+        <input
+          ref={pw}
+          id="input_pw"
+          type="password"
+          name="input_pw"
+          placeholder="비밀번호를 입력해주세요"
+          value={inputPw}
+          onChange={changePw}
+        />
+        <input
+          ref={pw_chk}
+          id="input_pw_chk"
+          type="password"
+          name="input_pw_chk"
+          placeholder="비밀번호를 확인해주세요"
+          value={inputPwChk}
+          onChange={changePwChk}
+        />
+        <input
+          ref={username}
+          id="input_name"
+          type="text"
+          name="input_name"
+          placeholder="이름을 입력해주세요"
+          value={inputName}
+          onChange={changeName}
+        />
+        <div className={styles.inputBox}>
+          <label htmlFor="input_birthDate">생년월일</label>
           <input
             ref={birth_date}
             id="input_birthDate"
@@ -137,36 +130,25 @@ export default function SignUp(props) {
             onChange={changeBirth}
           />
         </div>
-        <div>
-          <label htmlFor="input_gender">성별: </label>
-          <label htmlFor="female">여성</label>
-          <input
-            id="female"
-            type="radio"
-            name="input_gender"
-            value="female"
-            className={styles.radio}
-            onChange={changeGender}
-            checked={inputGender === 'female' ? true : false}
-          />
-          <label htmlFor="male">남성</label>
-          <input
-            id="male"
-            type="radio"
-            name="input_gender"
-            value="male"
-            className={styles.radio}
-            onChange={changeGender}
-            checked={inputGender === 'male' ? true : false}
-          />
+
+        <div className={styles.inputBox}>
+          <label>성별</label>
+          <div className={styles.select}>
+            <select value={inputGender} onChange={changeGender}>
+              <option value="female">여성</option>
+              <option value="male">남성</option>
+            </select>
+          </div>
         </div>
-        <div>
-          <button type="button" onClick={onClickSignUp}>
-            회원가입
-          </button>
-        </div>
+
+        <button type="button" onClick={onClickSignUp}>
+          회원가입
+        </button>
+
         <p>{error ? errorMsg : null}</p>
       </div>
+
+      <a href="/login">Login Here</a>
     </div>
   );
 }
